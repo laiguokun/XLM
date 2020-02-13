c43497f26220ba81d34dd41729c6f9e36981de15
Guokun Lai <glai1@la.lti.cs.cmu.edu>
2020-02-13 12:53:53 -0500

    update

--- online_mt_ro.sh
+++ online_mt_ro.sh
@@ -1,6 +1,7 @@
 { st=$(date '+%d-%m-%Y-%H:%M:%S');\
 folder_dir=/usr0/home/glai1/research/XLM/tmp/$st;\
-lang=de;\
+lang=ro;\
+folder=en-ro;\
 XLM_path=/usr0/home/glai1/research/XLM;\
 tool_path=$XLM_path/tools;\
 replace_punct=$tool_path/mosesdecoder/scripts/tokenizer/replace-unicode-punctuation.perl;\
@@ -10,9 +11,12 @@ tokenizer=$tool_path/mosesdecoder/scripts/tokenizer/tokenizer.perl;\
 detok_path=$tool_path/mosesdecoder/scripts/tokenizer/detokenizer.perl;\
 fastbpe=$tool_path/fastBPE/fast;\
 preprocess=$XLM_path/preprocess.py;\
-code_path=$XLM_path/de-en/codes_ende;\
-vocab_path=$XLM_path/de-en/vocab_ende;\
+code_path=$XLM_path/$folder/codes_en$lang;\
+vocab_path=$XLM_path/$folder/vocab_en$lang;\
 out_fn=${folder_dir}-hyp.txt;\
-$replace_punct | $norm_func -l $lang | $rem_no_print | $tokenizer -l $lang -no-escape -threads 8 | $fastbpe applybpe_stream $code_path | $XLM_path/translate.sh $out_fn $lang && \
+wmt16_scripts=$tool_path/wmt16-scripts
+NORMALIZE_ROMANIAN=$wmt16_scripts/preprocess/normalise-romanian.py
+REMOVE_DIACRITICS=$wmt16_scripts/preprocess/remove-diacritics.py
+$replace_punct | $norm_func -l $lang | $rem_no_print | $NORMALIZE_ROMANIAN | $REMOVE_DIACRITICS | $tokenizer -l $lang -no-escape -threads 8 | $fastbpe applybpe_stream $code_path | $XLM_path/translate.sh $out_fn $lang && \
 sed -i -r 's/(@@ )|(@@ ?$)//g' $out_fn && \
 cat $out_fn | perl $detok_path -q -l $lang; }
--- translate.sh
+++ translate.sh
@@ -1,3 +1,3 @@
 python /usr0/home/glai1/research/XLM/translate.py --exp_name translate \
---src_lang en --tgt_lang de --batch_size 256 \
---model_path /usr0/home/glai1/research/XLM/best-valid_en-de_mt_bleu.pth --output_path $1
+--src_lang $2 --tgt_lang en --batch_size 128 \
+--model_path /usr0/home/glai1/research/XLM/best-valid_en-ro_mt_bleu.pth --output_path $1
